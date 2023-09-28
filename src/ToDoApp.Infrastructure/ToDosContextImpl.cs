c297560a593e3c1cb70e245e7a722a5d661a81af
Joshua Hogsett <jthogsett@gmail.com>
2023-09-28 11:31:24 -0700

    delete todos from db when flagged

--- src/ToDoApp.Infrastructure/ToDosContextImpl.cs
+++ src/ToDoApp.Infrastructure/ToDosContextImpl.cs
@@ -7,6 +7,18 @@ public class ToDosContextImpl : ToDosContext
 {
     public ToDosContextImpl(DbContextOptions<ToDosContextImpl> options) : base(options)
     {
+        SavingChanges += DeleteToDos;
+    }
+
+    private void DeleteToDos(object? sender, SavingChangesEventArgs e)
+    {
+        foreach (var toDoEntry in ChangeTracker.Entries<Domain.Entities.ToDo>())
+        {
+            if (toDoEntry.Entity.IsDeleted)
+            {
+                toDoEntry.State = EntityState.Deleted;
+            }
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
